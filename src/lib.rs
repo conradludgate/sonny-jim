1baed15dfb86e83ac1bf44d54577c4b716d562f6
Conrad Ludgate <conradludgate@gmail.com>
2024-10-20 17:24:55 +0100

    write string parser and interner

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,15 @@ extern crate alloc;
 #[macro_use(dbg)]
 extern crate std;
 
+use alloc::boxed::Box;
+use alloc::string::String;
 use alloc::vec::Vec;
-use core::ops::{Range, RangeFrom};
+use core::hash::BuildHasher;
+use core::ops::{Index, Range, RangeFrom};
+use hashbrown::hash_table::Entry;
+use hashbrown::HashTable;
+use indexmap::IndexMap;
+use thin_vec::ThinVec;
 
 use logos::Logos;
 
@@ -84,8 +91,150 @@ pub struct Error {
     context: ContextItem,
 }
 
-pub fn parse(s: &str) -> Result<(), Error> {
-    let mut lexer = Token::lexer(s);
+#[derive(Debug, Clone)]
+pub enum Value {
+    Bool(bool),
+    Null,
+    Number(Range<u32>),
+    String(StringKey),
+    Object(Box<IndexMap<StringKey, Value, foldhash::quality::RandomState>>),
+    Array(ThinVec<Value>),
+}
+
+#[derive(Debug, Clone)]
+pub struct StringKey(Range<u32>);
+
+struct Scratch<'a> {
+    src: &'a str,
+    scratch: String,
+}
+
+pub struct Interner<'a> {
+    scratch: Scratch<'a>,
+    hasher: foldhash::quality::RandomState,
+    table: HashTable<StringKey>,
+}
+
+impl<'a> Index<&StringKey> for Scratch<'a> {
+    type Output = str;
+
+    fn index(&self, index: &StringKey) -> &Self::Output {
+        let Range { start, end } = index.0;
+        if end < start {
+            &self.scratch[end as usize..start as usize]
+        } else {
+            &self.src[start as usize..end as usize]
+        }
+    }
+}
+
+impl<'a> Index<&StringKey> for Interner<'a> {
+    type Output = str;
+
+    fn index(&self, index: &StringKey) -> &Self::Output {
+        &self.scratch[index]
+    }
+}
+
+impl<'a> Interner<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Self {
+            scratch: Scratch {
+                src,
+                scratch: String::new(),
+            },
+            hasher: Default::default(),
+            table: HashTable::new(),
+        }
+    }
+
+    fn intern(&mut self, span: Range<u32>) -> StringKey {
+        let Self {
+            scratch,
+            hasher,
+            table,
+        } = self;
+
+        // check that this actually points to a string...
+        debug_assert_eq!(scratch.src.as_bytes()[span.start as usize], b'"');
+        debug_assert_eq!(scratch.src.as_bytes()[span.end as usize], b'"');
+        debug_assert!(span.start + 2 <= span.end);
+
+        let mut start = span.start as usize + 1;
+        let end = span.end as usize - 1;
+
+        let scratch_start = scratch.scratch.len();
+
+        loop {
+            let b = scratch.src.as_bytes();
+            let Some(escape) = memchr::memchr(b'\\', &b[start..end]) else {
+                break;
+            };
+            scratch
+                .scratch
+                .push_str(&scratch.src[start..start + escape]);
+
+            start += 1;
+
+            match b[start] {
+                b'"' => scratch.scratch.push('"'),
+                b'\\' => scratch.scratch.push('\\'),
+                b'/' => scratch.scratch.push('/'),
+                b'b' => scratch.scratch.push('\x08'),
+                b'f' => scratch.scratch.push('\x0c'),
+                b'n' => scratch.scratch.push('\n'),
+                b'r' => scratch.scratch.push('\r'),
+                b't' => scratch.scratch.push('\t'),
+                b'u' => {
+                    // TODO: is this even right???
+                    // \u1234 -> U+1234
+                    // TODO: maybe support utf16
+
+                    let hex_bytes: [u8; 4] = *b
+                        .first_chunk()
+                        .expect("logos should have validated that 4 hex bytes follow the \\u");
+                    let mut code = [0; 2];
+                    hex::decode_to_slice(hex_bytes, &mut code)
+                        .expect("should have validated the hex already");
+
+                    if let Some(c) = char::from_u32(u16::from_be_bytes(code) as u32) {
+                        scratch.scratch.push(c);
+                    } else {
+                        todo!("error")
+                    }
+                }
+                _ => unreachable!("escape character has been validated by logos"),
+            }
+        }
+
+        let span;
+        let str;
+        if scratch_start < scratch.scratch.len() {
+            scratch.scratch.push_str(&scratch.src[start..end]);
+            span = scratch.scratch.len() as u32..scratch_start as u32;
+            str = &scratch.scratch[scratch_start..];
+        } else {
+            span = start as u32..end as u32;
+            str = &scratch.src[start..end];
+        };
+
+        let hash = hasher.hash_one(str);
+        match table.entry(
+            hash,
+            |key| &scratch[key] == str,
+            |key| hasher.hash_one(&scratch[key]),
+        ) {
+            Entry::Occupied(occupied_entry) => {
+                scratch.scratch.truncate(scratch_start);
+                occupied_entry.get().clone()
+            }
+            Entry::Vacant(vacant_entry) => vacant_entry.insert(StringKey(span)).get().clone(),
+        }
+    }
+}
+
+pub fn parse(i: Interner<'_>) -> Result<Value, Error> {
+    let mut lexer = Token::lexer(i.scratch.src);
 
     let mut stack = vec![];
     let mut context = ContextItem::WaitingValue;
@@ -172,7 +321,7 @@ pub fn parse(s: &str) -> Result<(), Error> {
             },
         }
     }
-    Ok(())
+    Ok(Value::Null)
 }
 
 #[cfg(test)]
@@ -180,13 +329,15 @@ mod tests {
     use core::hint::black_box;
     use std::time::Instant;
 
+    use crate::Interner;
+
     #[test]
     fn bench_this() {
         let src = include_str!("../testdata/kubernetes-oapi.json");
 
         let start = Instant::now();
         for _ in 0..1000 {
-            black_box(crate::parse(black_box(src))).unwrap();
+            black_box(crate::parse(black_box(Interner::new(src)))).unwrap();
         }
         dbg!(start.elapsed() / 1000);
     }
@@ -226,6 +377,6 @@ mod tests {
         let first_half = "[".repeat(cool_factor);
         let second_half = "]".repeat(cool_factor);
         let input = std::format!("{first_half}{second_half}");
-        crate::parse(&input).unwrap();
+        crate::parse(Interner::new(&input)).unwrap();
     }
 }
